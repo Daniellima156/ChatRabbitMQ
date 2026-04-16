4e5c0813df73c88c26534d76b39dff4445a9b917
Ubuntu <ubuntu@ip-172-31-33-70.ec2.internal>
2026-04-16 02:13:31 +0000

    Finalizando a etapa 1 do projeto

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -1,34 +1,68 @@
 package br.ufs.dcomp.ChatRabbitMQ;
 
 import com.rabbitmq.client.*;
-
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
+import java.util.Scanner;
 import java.io.IOException;
 
 public class Chat {
 
+  private static String destino = "";
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setHost("3.95.195.126");
+    factory.setUsername("admin");
+    factory.setPassword("password");
     factory.setVirtualHost("/");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
     
-    String QUEUE_NAME = "minha-fila";
-                      //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.print("User: ");
+    String usuario = scan.nextLine();
+    
+    channel.queueDeclare(usuario, false, false, false, null);
     
     Consumer consumer = new DefaultConsumer(channel) {
-      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
-
-        String message = new String(body, "UTF-8");
-        System.out.println(message);
-
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        String msg = new String(body, "UTF-8");
+        System.out.println("\n" + msg);
+        if(destino.isEmpty()){
+          System.out.print("<< ");
+        } else {
+          System.out.print("@"+destino+"<< ");
+        }
       }
     };
-                      //(queue-name, autoAck, consumer);    
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
+    channel.basicConsume(usuario, true, consumer);
     
+    while(true){
+      if(destino.isEmpty()){
+        System.out.print("<< ");
+      } else {
+        System.out.print("@"+destino+"<< ");
+      }
+      
+      String msg = scan.nextLine();
+      
+      if(msg.startsWith("@")){
+        destino = msg.substring(1);
+      } else if(!destino.isEmpty()){
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"));
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy 'às' HH:mm");
+        String horario = now.format(formato);
+        String mensagemFormatada = "(" + horario + ") " + usuario + " diz: " + msg;
+        channel.basicPublish("", destino, null, mensagemFormatada.getBytes("UTF-8"));
+      } else {
+        break;
+      }
+    }
+    
+    channel.close();
+    connection.close();
+    scan.close();
   }
 }
